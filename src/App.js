ef8d62c23761718d8b8b4bbaf34a8e47261d01c3
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-08-06 17:47:46 +0400

    implemented loader error ready and start maincomponents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "setQuestions":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "start" };
+    default:
+      throw new Error("Action not found");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "setQuestions", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "start" && <Question />}
       </Main>
     </div>
   );
