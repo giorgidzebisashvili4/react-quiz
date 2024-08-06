74c6fb3e0865db48cc19c4d323cda6cfde2e86ef
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-08-06 15:46:40 +0400

    finished basic set up of useReducer

---
+++ src/DateCounter.js
@@ -0,0 +1,79 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { useState, useReducer } from "react";
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case "inc":
+      return state + 1;
+    case "dec":
+      return state - 1;
+    case "setCount":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+function DateCounter() {
+  // const [count, setCount] = useState(0);
+
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  const [step, setStep] = useState(1);
+
+  // This mutates the date object.
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  const dec = function () {
+    dispatch({ type: "dec" });
+    // setCount((count) => count - 1);
+    // setCount((count) => count - step);
+  };
+
+  const inc = function () {
+    dispatch({ type: "inc" });
+    // setCount((count) => count + 1);
+    // setCount((count) => count + step);
+  };
+
+  const defineCount = function (e) {
+    // setCount(Number(e.target.value));
+    console.log(e.target.value);
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
+  };
+
+  const defineStep = function (e) {};
+
+  const reset = function () {
+    // setCount(0);
+    setStep(1);
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+export default DateCounter;
