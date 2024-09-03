b00b82d28a41649981e0c19f0aad83eeb7a8e747
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-09-03 13:54:58 +0400

    finish quize page and restart button implemented

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,32 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = Math.round((points / maxPossiblePoints) * 100);
+
+  let emoji;
+  if (percentage >= 80) {
+    emoji = "🥇";
+  } else if (percentage >= 60) {
+    emoji = "🥈";
+  } else if (percentage >= 40) {
+    emoji = "🥉";
+  } else {
+    emoji = "🤷‍♂️";
+  }
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
