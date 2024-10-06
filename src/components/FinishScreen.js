0c40450f5ebd98b2a2e2869d37382a26e3bd7adc
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-10-06 19:32:03 +0400

    fixed high score bug on finish screen

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,7 +1,7 @@
 import { useQuiz } from "../contexts/QuizContext";
 
 function FinishScreen() {
-  const { points, maxPossiblePoints, highscore, dispatch } = useQuiz();
+  const { points, maxPossiblePoints, highScore, dispatch } = useQuiz();
   const percentage = Math.round((points / maxPossiblePoints) * 100);
 
   let emoji;
@@ -21,7 +21,7 @@ function FinishScreen() {
         <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
         {maxPossiblePoints}({Math.ceil(percentage)}%)
       </p>
-      <p className="highscore">(Highscore: {highscore} points)</p>
+      <p className="highscore">(High Score: {highScore} points)</p>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "restart" })}
